e680cd3c8c9116f622c7b28a448aafc453e76fbe
Vinit-070 <100857730+Vinit-070@users.noreply.github.com>
2023-07-08 11:31:25 +0530

    Add files via upload

---
+++ pr1_a.php
@@ -0,0 +1,7 @@
+// Author: Vinit Patel
+// Aim : PHP program to display today’s date in dd-mm-yyyy format.
+
+<?php
+$current_date = date('d-m-Y');
+echo "Today's Date : ".$current_date;
+?>
---
+++ pr1_b.php
@@ -0,0 +1,14 @@
+// Author: Vinit Patel
+// Aim : PHP program for swapping of two numbers.
+
+<?php
+$a = 7;
+$b = 11;
+echo "Before Swapping : a = ".$a;
+echo "Before Swapping : b = ".$b;
+$temp = $a;
+$a = $b;
+$b = $temp;
+echo "After Swapping : a = ".$a;
+echo "After Swapping : b = ".$b;
+?>
